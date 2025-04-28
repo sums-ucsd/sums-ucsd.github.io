93fdf1b338067e94df922cab336c02632de9fd93
harry-lons <harrylonsdale25@gmail.com>
2025-04-28 15:28:37 -0700

    converted to react

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+
+function About() {
+  return (
+    <>
+
+      <div class="container">
+        <div class="get-involved-image">
+          <img src="apm.jpg" class="sums-people" />
+        </div>
+
+        <div class="get-involved-content">
+          <h2>About Us</h2>
+          SUMS stands for the Society of Undergraduate Mathematics Students and we are the largest undergrad math organization here at UCSD. We aim to provide helpful workshops for all types of math majors as well as a way for the math community to come together. Our board meetings are at 4PM in APM 6402 on Mondays. Our events are mainly held in AP&M.
+
+          <h2> Membership Check</h2>
+          If you would like to check your membership status and if you are eligible for SUMS membership, you can fill out the form below. Please remember to delete your information after.
+
+          <iframe src="https://docs.google.com/spreadsheets/d/19-j42XJFwiYh4QqfMx8L-qHsXiUzeaa6eCvdzW-0MAE/edit?usp=sharing" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="500px" width="110%" allowfullscreen></iframe>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+
+function Home() {
+  return (
+    <>
+
+          <div>
+            <img src="main-logo.png" alt="sums-logo" class = 'main-image' />
+          </div>
+          <div class="button-container">
+            <a href="https://forms.gle/NTt7DFMNYkBJQx4y5" class="button-main">meeting sign-in</a>
+            <a href="https://forms.gle/zRqwfVnr4QXME5Xk6" class="button-main">event sign-in</a>
+            <a href="https://forms.gle/A1n3Bi3x1rN3pCHc7" class="button-main">membership application</a>
+          </div>
+    </>
+  );
+}
+
+export default Home;
