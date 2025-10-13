3bd33ca64c190e935521773c2a4d666b59a664d2
naomimetzler <nmetzler@ucsd.edu>
2025-10-13 13:39:28 -0700

    made list of board member profile info

---
+++ src/data/profiles.js
@@ -0,0 +1,138 @@
+import emilyImg from "../assets/images/EmilyHawksworth.jpeg";
+import harryImg from "../assets/images/HarryLonsdale.png";
+import kaylaImg from "../assets/images/KaylaTorresRodriguez.jpeg";
+import lalehImg from "../assets/images/LalehBoursalian.jpeg";
+import mattImg from "../assets/images/MattTokunaga.jpg";
+import mohitImg from "../assets/images/MohitDutta.jpg";
+import naomiImg from "../assets/images/NaomiMetzler.jpg";
+import ryanImg from "../assets/images/RyanBatubara.jpeg";
+import sahithImg from "../assets/images/SahithEdula.png";
+import thomasImg from "../assets/images/ThomasMadden.jpg";
+
+export const profiles = {
+    'harry-lonsdale': {
+        name: 'Harry Lonsdale',
+        title: 'President',
+        img: harryImg,
+        year: '4th',
+        college: 'Seventh',
+        major: 'Math-CS',
+        why: 'Free food. Just kidding, I wanted to connect with other math students and build a stronger community among undergrads in the department (while eating free food).',
+        hobbies: 'running, triathlon, watching English soccer, coding',
+        favoriteFields: 'algebra, graph theory',
+        favoriteClasses: 'Math 154'
+    },
+    'ryan-batubara': {
+        name: 'Ryan Yeshaya Batubara',
+        title: 'VP Internal',
+        img: ryanImg,
+        year: '4th',
+        college: 'Revelle',
+        major: 'Math-CS & Data Science',
+        why: 'Meet other math majors.',
+        hobbies: 'Classical guitar',
+        favoriteFields: 'Recreational math, graph theory.',
+        favoriteClasses: 'Math 87'
+    },
+    'emily-hawksworth': {
+        name: 'Emily Hawksworth',
+        title: 'VP External',
+        img: emilyImg,
+        year: '4th',
+        college: 'Muir',
+        major: 'Mathematics - Probability and Statistics',
+        why: 'To meet other math students and get involved!',
+        hobbies: 'Hiking and golfing',
+        favoriteFields: 'Statistics',
+        favoriteClasses: 'Math 109, 181A & 181B'
+    },
+    'mohit-dutta': {
+        name: 'Mohit Dutta',
+        title: 'Secretary',
+        img: mohitImg,
+        year: '3rd',
+        college: 'Seventh',
+        major: 'Math-CS, Economics',
+        why: 'To meet more friends in the math department!',
+        hobbies: 'Reading, climbing, singing, being chronically online',
+        favoriteFields: 'Graph Theory, Theoretical Econ',
+        favoriteClasses: 'Math 170A, Math 100B, Math 158'
+    },
+    'sahith-edula': {
+        name: 'Sahith Edula',
+        title: 'Treasurer',
+        img: sahithImg,
+        year: '3rd',
+        college: 'Muir',
+        major: 'Math and Economics (double major)',
+        why: 'To make Math a more welcoming community for everyone!',
+        hobbies: 'Reading, learning languages, sudoku puzzles',
+        favoriteFields: 'Probability and game theory',
+        favoriteClasses: 'Math 140, Math 280'
+    },
+    'thomas-madden': {
+        name: 'Thomas Madden',
+        title: 'Events and Workshops Chair',
+        img: thomasImg,
+        year: '3rd',
+        college: 'Seventh',
+        major: 'Mathematics',
+        why: 'I joined SUMS to be apart of a community of students interested in advanced mathematics, and to have the opportunity to contribute to the creation/planning of math-related competitions and workshops.',
+        hobbies: 'basketball, drawing, reading',
+        favoriteFields: 'Probability, Statistics, and Number Theory',
+        favoriteClasses: 'Math 140A, Math 180A, Math 105'
+    },
+    'kayla-rodriguez': {
+        name: 'Kayla Rodriguez',
+        title: 'Publicity Chair',
+        img: kaylaImg,
+        year: '3rd',
+        college: 'Sixth',
+        major: 'Applied Mathematics',
+        why:
+          'I joined SUMS to gain a sense of community at UCSD, specifically within those who share the love for math as well!',
+        hobbies:
+          'Working out, practicing piano/flute/guitar (when not crunching numbers)',
+        favoriteFields: 'Differential equations and linear algebra',
+        favoriteClasses: 'Math 102'
+    },
+    'matt-tokunaga': {
+        name: 'Matt Tokunaga',
+        title: 'Historian',
+        img: mattImg,
+        year: '4th',
+        college: 'Revelle',
+        major: 'Pure math + Data Science',
+        why: 'To make friends :D😉',
+        hobbies: 'Watching baseball and basketball, hoopin, video games',
+        favoriteFields: 'Functional analysis',
+        favoriteClasses: 'Math 140B, Math 100C'
+    },
+    'laleh-boursalian': {
+        name: 'Laleh Boursalian',
+        title: 'Media Chair',
+        img: lalehImg,
+        year: '4th',
+        college: 'Warren',
+        major: 'Applied Mathematics',
+        why:
+          'I wanted to meet new people with similar academic interests as me, and get involved with on campus events.',
+        hobbies: 'cooking, reading, playing The Sims 4 and Minecraft',
+        favoriteFields: 'Calculus',
+        favoriteClasses: 'Math 142A, Math 170A'
+    },
+    'naomi-metzler': {
+        name: 'Naomi Metzler',
+        title: 'Tech Chair',
+        img: naomiImg,
+        year: '3rd',
+        college: 'Eighth',
+        major: 'Math-CS',
+        why: 'I wanted to make friends with other math people!',
+        hobbies: 'Running, art/sewing, thrifting, and rollerblading',
+        favoriteFields: 'Graph theory',
+        favoriteClasses: 'Math 100B, Math 154',
+    }
+}
+
+
