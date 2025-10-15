d4f5ec8669c1b9e8ebacedaa7b8a32bfe1f74706
naomimetzler <nmetzler@ucsd.edu>
2025-10-15 13:01:41 -0700

    updated home for react

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,39 @@
+import React from "react";
+import mainLogo from "../assets/images/main_logo.png";
 
 function Home() {
   return (
     <>
+      <div>
+        <img src={mainLogo} alt="SUMS logo" className="main-image" />
+      </div>
 
-          <div>
-            <img src="main-logo.png" alt="sums-logo" class = 'main-image' />
-          </div>
-          <div class="button-container">
-            <a href="https://forms.gle/NTt7DFMNYkBJQx4y5" class="button-main">meeting sign-in</a>
-            <a href="https://forms.gle/zRqwfVnr4QXME5Xk6" class="button-main">event sign-in</a>
-            <a href="https://forms.gle/A1n3Bi3x1rN3pCHc7" class="button-main">membership application</a>
-          </div>
+      <div className="button-container">
+        <a
+          href="https://forms.gle/NTt7DFMNYkBJQx4y5"
+          className="button-main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          meeting sign-in
+        </a>
+        <a
+          href="https://forms.gle/zRqwfVnr4QXME5Xk6"
+          className="button-main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          event sign-in
+        </a>
+        <a
+          href="https://forms.gle/A1n3Bi3x1rN3pCHc7"
+          className="button-main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          membership application
+        </a>
+      </div>
     </>
   );
 }
